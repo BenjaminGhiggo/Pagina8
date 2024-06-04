fcdf456d7314e7127a4e765bb3df6bb0549cacdb
Ben <victor.ghiggo.d@uni.pe>
2024-06-04 09:25:18 -0500

    Ajuste del titulo del encabezado

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Pagina8/",
-  plugins: [react()],
+  base: "/Pagina8/",
+  plugins: [react()]
 })
