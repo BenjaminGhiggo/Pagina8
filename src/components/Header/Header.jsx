657b36aa8e7a1ee441941e4f469cce4d3a3b3b9d
Ben <victor.ghiggo.d@uni.pe>
2024-06-04 09:48:45 -0500

    Commit número 7: Ajuste de encabezado para vista móvil

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
                 {/* Puedes dejar este espacio en blanco o poner un logo adicional */}
             </div>
             <div className="header-title">
-                <h1>Caldora múltiples funciones</h1>
+                <h1>Caldora2 múltiples funciones</h1>
             </div>
             <div className="header-right">
                 <button className="header-button">Sign Up</button>
